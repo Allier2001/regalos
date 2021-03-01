28eef3979716fb096c1a274eb8633e06ac47b131
Emilio Ordoñez <36750893+Allier2001@users.noreply.github.com>
2021-03-01 08:05:59 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+}); 
+
+ScrollReveal().reveal('.container', { delay: 500});
+ScrollReveal().reveal('.showcase', { delay: 500});
+ScrollReveal().reveal('.cards', { delay: 500});
+ScrollReveal().reveal('.cards-banner-one', { delay: 500});
+ScrollReveal().reveal('.cards-banner-two', { delay: 500});
+ScrollReveal().reveal('.footer-links', { delay: 500});
+ScrollReveal().reveal('.footer', { delay: 500});
+
